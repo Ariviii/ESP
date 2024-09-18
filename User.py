fff5ba66b3ad942d9401b1810dfae5f503e5ca16
Ariviii <162411005+Ariviii@users.noreply.github.com>
2024-09-18 15:14:01 +0530

    Update User.py
    Change the localhost IP

--- User.py
+++ User.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 def receive_sensor_data():
     data = request.get_json()
     print("\n[<---]Reciving Data: ",type(data),"\n")
-    print(data)
+    print("\n[Live Data] :",data)
     if not data:
         return jsonify({"error": "Invalid data format"}), 415  # Handle unsupported media type
     return jsonify({"status": "success", "received_data": data})
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=6001, debug=True)
+    app.run(host="0.0.0.0", port=6001, debug=True)
